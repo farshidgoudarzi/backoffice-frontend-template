1230dae42bab4bb2b2169771e29e816b60b414cf
Farshid Goudarzi <76595534+farshidgoudarzi@users.noreply.github.com>
2024-10-20 15:36:37 +0200

    feat(sidebar): adds sample sidebar

--- src/app.tsx
+++ src/app.tsx
@@ -2,51 +2,54 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Button } from "./components/ui/button";
+import Layout from "./layout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex space-x-4 mb-8">
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-          className="hover:opacity-80 transition-opacity"
-        >
-          <img src={viteLogo} className="h-24 w-24" alt="Vite logo" />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          className="hover:opacity-80 transition-opacity"
-        >
-          <img
-            src={reactLogo}
-            className="h-24 w-24 animate-spin-slow"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1 className="text-4xl font-bold mb-8">Vite + React</h1>
-      <div className="p-8 rounded-lg shadow-md flex flex-col items-center bg-muted">
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-          variant="default"
-          size="lg"
-          className="mb-4"
-        >
-          count is {count}
-        </Button>
-        <p className="text-center">
-          Edit <code className="p-1 rounded">src/App.tsx</code> and save to test
-          HMR
+    <Layout>
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="flex space-x-4 mb-8">
+          <a
+            href="https://vitejs.dev"
+            target="_blank"
+            className="hover:opacity-80 transition-opacity"
+          >
+            <img src={viteLogo} className="h-24 w-24" alt="Vite logo" />
+          </a>
+          <a
+            href="https://react.dev"
+            target="_blank"
+            className="hover:opacity-80 transition-opacity"
+          >
+            <img
+              src={reactLogo}
+              className="h-24 w-24 animate-spin-slow"
+              alt="React logo"
+            />
+          </a>
+        </div>
+        <h1 className="text-4xl font-bold mb-8">Vite + React</h1>
+        <div className="p-8 rounded-lg shadow-md flex flex-col items-center bg-muted">
+          <Button
+            onClick={() => setCount((count) => count + 1)}
+            variant="default"
+            size="lg"
+            className="mb-4"
+          >
+            count is {count}
+          </Button>
+          <p className="text-center">
+            Edit <code className="p-1 rounded">src/App.tsx</code> and save to
+            test
+          </p>
+        </div>
+        <p className="mt-8 text-sm">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="mt-8 text-sm">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/components/AppSidebar/index.tsx
@@ -0,0 +1,63 @@
+import { Home, Settings, LogOut } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Organization</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <LogOut />
+              <span>Logout</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
